0fe4c71547a4b742202868fb618eece7c55e386a
Ketan Tetry <k10tetry@gmail.com>
2024-07-16 01:23:30 +0530

    solve publishing issue

--- auto-complete-box/build.gradle.kts
+++ auto-complete-box/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -24,11 +25,23 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.k10tetry"
+                artifactId = "auto-complete-box"
+                version = "1.0"
+            }
+        }
     }
 }
 
